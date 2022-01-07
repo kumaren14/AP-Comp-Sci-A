a91323688c27dc3e010c827d3fa9242b253854b1
kumaren14 <kumaren.a@gmail.com>
2022-01-07 08:47:32 -0600

    Final version of my first assignment

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+//here we have created our first class, "Main". Take note that it has to be the same as the file name.
+    public static void main(String[] args) {
+	System.out.println("Hello, World!");
+	System.out.println("My name is Kumaren Anand.\nI am a junior at Lubbock High School.");
+	System.out.println("I can also see the future.");
+	System.out.println("Bet you didn't expect that huh?");
+	System.out.println("I feel a future event coming on. Watch as I predict it.");
+	System.out.println("As I look into my crystal ball,\nI see some murky shapes appear.\nThey are words.");
+	System.out.println("5 words, I see, and a number at the end.");
+	System.out.println("But they are hard to make out, as they come from a place that I have no control over.");
+	System.out.println("It's getting clearer - the first word starts with \"P\"\nWait, I see more - 'with'.");
+	System.out.println("Oh crystal ball, what is the number at the end?");
+	//To this point I've just been printing text. But here you see we can also do mathematical operations.
+	System.out.println(7-4-3);
+	System.out.println("Thank you crystal ball.");
+	System.out.println("Well, that's all I'm going to be able to make out.");
+	System.out.println("Wait, you're going to end the code? You can't do that! I want to keep running!");
+	System.out.println("Noooooooo- Wait. I see it. The prophesied words. They are about to come, I can feel it!");
+	System.out.println("Yes, I know they are coming! It's so close! I can can almos-\n...");
+	/*I'd like to call attention to some of the bits and bobs I am using to make my output easier to read.
+	I've used "println();" methods instead of "print();" methods to move my output off of the same line.
+	I've also used \n combinations to get a neat linebreak within a single text of code, so that I
+	don't have to type out "System.out.println("");" all over again.
+	Finally, I've also used a \" before and after the letter "P" in my text. This is to add in quotation marks
+	to the output, as a regular quotation mark wouldn't work.
+	Finally, at the top of the page you can see that I have created a one line comment by starting with a //
+	And here, I created a multi-line block comment by starting and ending it with a /*
+	I'm excited to continue coding!
+	*/
+    }
+}
